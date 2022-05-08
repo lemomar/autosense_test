474a5ea443eb62387cdaf4dfb7406136fa27523b
lemomar <lemomar@users.noreply.github.com>
2022-05-08 13:47:11 +0200

    🎉 Adress & City fetching operational

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { DocumentSnapshot } from 'firebase-admin/firestore';
 import { Router } from "express";
+import axios from 'axios';
 
 
 import db from "../firebase";
@@ -16,7 +17,7 @@ router.get("/", async (req: any, res: any) => {
       id: doc.id,
       ...doc.data(),
     }));
-    res.json({ stations });
+    res.status(200).json({ stations });
   } catch (error) {
     res.status(400).json({ error: "An error occured. Please try again." });
   }
@@ -27,6 +28,9 @@ router.post("/new-station", async (req: any, res: any) => {
     const newStation: Station = req.body as Station;
     const doc = await db.collection("stations").doc(req.body.id).get();
     if (doc.exists) throw new StationExistsError();
+    const resultsFromGMaps = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.body.latitude},${req.body.longitude}&key=${process.env.GMAPP_API_KEY}`);
+    newStation.city = resultsFromGMaps.data.results.find((element: any) => element.types.includes("locality"))?.address_components[0]?.long_name || "";
+    newStation.address = resultsFromGMaps.data.results.find((element: any) => element.types.includes("plus_code"))?.formatted_address || "";
     const id = req.body.id;
     delete newStation.id;
     await db.collection("stations").doc(id).set({ ...newStation });
@@ -36,6 +40,7 @@ router.post("/new-station", async (req: any, res: any) => {
       res.status(400).json({ error: error.message });
       return;
     }
+    console.log(error);
     res.status(400).json({ error: "An error occured. Please try again." });
   }
 });
