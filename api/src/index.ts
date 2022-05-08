33072ab3b9219f308c6191502581222737befcf1
lemomar <lemomar@users.noreply.github.com>
2022-05-08 05:04:11 +0200

    🚧 Google Cloud Setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import { initializeApp } from 'firebase-admin/app';
+import admin from 'firebase-admin';
+
+var serviceAccount:string = process.env.GOOGLE_APPLICATION_CREDENTIALS || "";
+
+initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+    projectId: "auto-sense-test",
+});
 
 const app = express();
 
